3fccd1a3bcd3964ea9a5e0792ab35c96ae11d826
wintersxul <127469851+wintersxul@users.noreply.github.com>
2023-12-29 17:26:09 +0700

    Update main.cpp

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,3 +1,14 @@
+#include "Point.h"
+#include <iostream>
+
 int main() {
+	Point point;
+	std::ifstream file("InputPoints.txt");
+	if (!file.is_open())
+		return -1;
+	point.Load(file);
+	point.PrintPoints(std::cout);
+	point.DeletePoints();
+	file.close();
 	return 0;
 }
